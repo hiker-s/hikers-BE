963d18b9fd6d0aa5eb910538afcd5b2881a2e958
SangHee-Hong <hongdabagi@gmail.com>
2025-04-23 22:30:21 +0900

    Init : 프로젝트 생성

---
+++ src/main/java/com/hikers/hikemate/HikemateApplication.java
@@ -0,0 +1,13 @@
+package com.hikers.hikemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hikers/hikemate/HikemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hikers.hikemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
