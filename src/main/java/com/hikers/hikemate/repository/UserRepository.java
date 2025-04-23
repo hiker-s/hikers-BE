3be2b3365f31c6590a6898bce8cbb93f2112a547
SangHee-Hong <hongdabagi@gmail.com>
2025-04-24 02:52:00 +0900

    Feat: UserRespository로 아이디/이메일별 사용자 조회

---
+++ src/main/java/com/hikers/hikemate/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.hikers.hikemate.repository;
+
+import com.hikers.hikemate.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    //아이디 또는 이메일로 사용자 찾기 ( 이메일도 포함인가?)
+    Optional<User> findByUsername(String user_id);
+
+    Optional<User> fingByEmail(String email);
+}
