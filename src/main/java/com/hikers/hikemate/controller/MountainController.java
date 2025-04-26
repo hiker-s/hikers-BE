d84c20107722d8c2fb02e78de035da97151e9e90
mxinseo <kyungminseo10@gmail.com>
2025-04-26 18:40:54 +0900

    Fix : AllArgsConstructor, RequiredArgsConstructor로 생성자 대체

--- src/main/java/com/hikers/hikemate/controller/MountainController.java
+++ src/main/java/com/hikers/hikemate/controller/MountainController.java
@@ -2,6 +2,7 @@ package com.hikers.hikemate.controller;
 
 import com.hikers.hikemate.dto.mountain.MountainGetAllResponseDto;
 import com.hikers.hikemate.service.MountainService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,17 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/mountain")
 public class MountainController {
 
     private final MountainService mountainService;
 
-    public  MountainController(MountainService mountainService) {
-        this.mountainService = mountainService;
-    }
-
     @GetMapping("/list")
-    public List<MountainGetAllResponseDto> mountainGetAllResponseDtos() {
+    public List<MountainGetAllResponseDto> mountainGetAll() {
         return mountainService.getAllMountains();
     }
 }
--- src/main/java/com/hikers/hikemate/dto/mountain/MountainGetAllResponseDto.java
+++ src/main/java/com/hikers/hikemate/dto/mountain/MountainGetAllResponseDto.java
@@ -1,6 +1,7 @@
 package com.hikers.hikemate.dto.mountain;
 
 import com.hikers.hikemate.dto.course.CourseDetailDto;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,16 +9,10 @@ import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class MountainGetAllResponseDto {
     private Long id;
     private String mntName;
     private String mntInfo;
     private List<CourseDetailDto> courses;
-
-    public MountainGetAllResponseDto(Long id, String mntName, String mntInfo, List<CourseDetailDto> courses) {
-        this.id = id;
-        this.mntName = mntName;
-        this.mntInfo = mntInfo;
-        this.courses = courses;
-    }
 }
