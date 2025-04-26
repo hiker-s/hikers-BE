9ee27c1480681be6b4acae12ab1a7d972e2628f9
SangHee-Hong <hongdabagi@gmail.com>
2025-04-26 23:29:43 +0900

    Feat: 스탬프 기능 구현

---
+++ src/main/java/com/hikers/hikemate/dto/StampResponseDTO.java
@@ -0,0 +1,28 @@
+package com.hikers.hikemate.dto;
+
+import com.hikers.hikemate.entity.Stamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+public class StampResponseDTO {
+    private Long stampId;          // 스탬프 ID
+    private String courseName;     // 코스 이름
+    private BigDecimal courseLat;  // 코스 위도
+    private BigDecimal courseLng;  // 코스 경도
+    private int levelWeight;       // 레벨 가중치
+
+    // Stamp 엔티티를 받아서 DTO로 변환하는 메서드
+    public static StampResponseDTO fromEntity(Stamp stamp) {
+        return new StampResponseDTO(
+                stamp.getId(),
+                stamp.getCourse().getCourseName(),
+                stamp.getCourse().getCourseLastLat(),
+                stamp.getCourse().getCourseLatLng(),
+                stamp.getLevelWeight()
+        );
+    }
+}
--- src/main/java/com/hikers/hikemate/repository/CourseRepository.java
+++ src/main/java/com/hikers/hikemate/repository/CourseRepository.java
@@ -3,5 +3,9 @@ package com.hikers.hikemate.repository;
 import com.hikers.hikemate.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+
 }
