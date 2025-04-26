52a4480cd9da484712e6b448866f3acc179f9b9a
mxinseo <kyungminseo10@gmail.com>
2025-04-26 18:44:33 +0900

    Fix : 산 목록 반환 SuccessDTO로 맞춤

--- src/main/java/com/hikers/hikemate/controller/MountainController.java
+++ src/main/java/com/hikers/hikemate/controller/MountainController.java
@@ -1,8 +1,10 @@
 package com.hikers.hikemate.controller;
 
+import com.hikers.hikemate.dto.base.SuccessResponseDTO;
 import com.hikers.hikemate.dto.mountain.MountainGetAllResponseDto;
 import com.hikers.hikemate.service.MountainService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,11 @@ public class MountainController {
     private final MountainService mountainService;
 
     @GetMapping("/list")
-    public List<MountainGetAllResponseDto> mountainGetAll() {
-        return mountainService.getAllMountains();
+    public ResponseEntity<?> mountainGetAll() {
+        List<MountainGetAllResponseDto> mountainList = mountainService.getAllMountains();
+        SuccessResponseDTO<List<MountainGetAllResponseDto>> response =
+                new SuccessResponseDTO<>(200, "산 리스트 조회 성공", mountainList);
+
+        return ResponseEntity.ok(response);
     }
 }
